dfb939e2d63052e7191ef90a50719719be410363
Sercan Efe Karaman <70342545+sercanefe@users.noreply.github.com>
2021-10-17 19:56:43 +0300

    Add files via upload

---
+++ adSoyadNumaraAlg.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 27 23:48:07 2021
+
+@author: sercan
+"""
+
+ad = input("Adınız? ")
+soyad = input("Soyadınız? ")
+numara = input("Numaranız? ")
+
+faktor1 = "Adı: "
+faktor2 = "Soyadı: "
+faktor3 = "Numarası: "
+
+print(faktor1 + ad)
+print(faktor2 + soyad)
+print(faktor3 + numara)
